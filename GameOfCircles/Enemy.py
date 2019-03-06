482897d04a8be04c914438444dc5d926a8ee1000
J.W. Clark <jwclark@rockhursths.edu>
2019-03-05 23:34:28 -0600

    Implement multiple inheritance

--- GameOfCircles/Enemy.py
+++ GameOfCircles/Enemy.py
@@ -1,24 +1,16 @@
 import SpriteManager # module, not class
 from Sprite import Sprite
 from Bullet import Bullet
+from Shooter import Shooter
 
-class Enemy(Sprite):
+class Enemy(Shooter, Sprite):
     
     speed = 8
     diameter = 50
     c = color(0,0,255)
         
     def move(self):
+        super(Enemy, self).move()
         self.x += self.speed
         if self.x < 0 or self.x > width:
             self.speed *= -1
-        
-        vector = self.aim(SpriteManager.getPlayer())
-        self.fire(vector)
-        
-    def aim(self, target):
-        # solve unit vector problem here
-        return PVector(0, 10) # use unit vector values here
-    
-    def fire(self, vector):
-        SpriteManager.spawn(Bullet(self.x, self.y, vector, self.team))
--- GameOfCircles/RaindropShooter.py
+++ GameOfCircles/RaindropShooter.py
@@ -1,11 +1,11 @@
 import math
 import SpriteManager
 
-from Sprite import Sprite
+from Shooter import Shooter
 from Raindrop import Raindrop
 from Bullet import Bullet
 
-class RaindropShooter(Raindrop):
+class RaindropShooter(Raindrop, Shooter):
     speed = 2
     diameter = 50
     c = color(255, 255, 0)
@@ -13,20 +13,5 @@ class RaindropShooter(Raindrop):
     wait = 700
 
     def move(self):
-        super(RaindropShooter, self).move()
-        vector = self.aim(SpriteManager.getPlayer())
-        self.fire(vector)
-        
-    def aim(self, target):
-        distance = dist(target.x, target.y, self.x, self.y)
-        xComponent = target.x - self.x
-        yComponent = target.y - self.y
-        f = 7
-        if distance == 0:
-            distance = 0.01
-        return PVector(xComponent / distance * f, yComponent / distance * f)
-            
-    def fire(self, vector):
-        if millis() - self.mark > self.wait:
-            SpriteManager.spawn(Bullet(self.x, self.y, vector, self.team))
-            self.mark = millis()
+        Raindrop.move(self)
+        Shooter.move(self)
